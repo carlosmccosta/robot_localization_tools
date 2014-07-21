3954006860ff7e3a6b45d74def83530c3c6c55b1
Carlos Miguel Correia da Costa <carloscosta.cmcc@gmail.com>
2014-07-21 13:37:37 +0100

    Added possibility to run system command after path is completed.

--- src/twist_publisher_node.cpp
+++ src/twist_publisher_node.cpp
@@ -8,6 +8,8 @@
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   <includes>   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 #include <ros/ros.h>
 #include <robot_localization_error/twist_publisher.h>
+#include <cstdlib>
+#include <sstream>
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   </includes>  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
@@ -18,9 +20,21 @@ int main(int argc, char** argv) {
 
 	ros::NodeHandlePtr node_handle(new ros::NodeHandle());
 	ros::NodeHandlePtr private_node_handle(new ros::NodeHandle("~"));
+
 	robot_localization_tools::TwistPublisher twist_publisher;
 	twist_publisher.setupConfigurationFromParameterServer(node_handle, private_node_handle);
 	twist_publisher.publishTwistFromParameterServer(private_node_handle);
+
+	if (argc > 1) {
+		std::stringstream ss;
+		ss << argv[1];
+		for (int i = 2; i < argc; ++i) {
+			ss << " " << argv[i];
+		}
+
+		ROS_INFO_STREAM("Calling system command: " << ss.str());
+		std::system(ss.str().c_str());
+	}
 
 	return 0;
 }
